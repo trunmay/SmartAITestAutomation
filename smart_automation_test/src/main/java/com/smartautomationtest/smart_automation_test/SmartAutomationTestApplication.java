2a224cc2bb910d0cd69cbe64540c6401b064fbd0
Trunmay Deshpande <trunmay1234@gmail.com>
2026-06-14 18:51:36 +0530

    Added a spring project

---
+++ smart_automation_test/src/main/java/com/smartautomationtest/smart_automation_test/SmartAutomationTestApplication.java
@@ -0,0 +1,13 @@
+package com.smartautomationtest.smart_automation_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartAutomationTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartAutomationTestApplication.class, args);
+	}
+
+}
---
+++ smart_automation_test/src/test/java/com/smartautomationtest/smart_automation_test/SmartAutomationTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartautomationtest.smart_automation_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartAutomationTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
